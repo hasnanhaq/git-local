85ff9fde4ea4a73375302f8518d91676c0eb1189
hasnanhaq <hasnanhaq.app@gmail.com>
2019-09-25 00:29:12 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,12 @@
 <body>
 	<h1>hellpww</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae minus repellat dolorum, vero voluptates aperiam dignissimos nam perspiciatis maiores tenetur sunt, aspernatur quibusdam et aut laudantium dolore blanditiis error debitis!</p>
+	<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+		</ul>	
 </body>
 </html>
